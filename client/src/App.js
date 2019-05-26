fa4fb4083dcc2d56cda5b3cd8b143c00d3ec8efe
m-tartag <mattbrs51@gmail.com>
2019-05-25 23:36:05 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="">
         <Navigation />
         <Body />
         <Foot />
