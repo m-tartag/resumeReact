47f63e470dbb091394666fe98a0155f12e5eeea6
m-tartag <mattbrs51@gmail.com>
2019-06-22 02:17:41 -0400

    update

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () =>
-  console.log(chalk.yellow.inverse(`Serverr Connected [Port: ${PORT}])`))
+  console.log(chalk.yellow.inverse(`Server Connected [Port: ${PORT}])`))
 );
